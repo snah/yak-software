e0aa6eae97f5ef759adea6442309a45d102df30a
Hans Maree <hans.maree@gmail.com>
2017-05-22 12:25:29 +0200

    Full (temporary) intergration test for main function.

--- yak_server/test/integration/test_main.py
+++ yak_server/test/integration/test_main.py
@@ -1,5 +1,6 @@
 import unittest
 import unittest.mock
+from unittest.mock import call
 
 from test import util
 
@@ -12,9 +13,12 @@ class TestMain(util.TestCase):
     mocks the internal representation of USB devices and with unit
     test for these classes.
     """
+
+    ITERATIONS = 5
+
     def setUp(self):
-        self.mock_usb = self.start_patch('yak_server.__main__.usb').mock
-        self.start_patch('yak_server.__main__.server_running', new=self.run_for_iterations(5))
+        self.mock_usb = self.start_patch('yak_server.__main__.usb', new=unittest.mock.MagicMock()).mock
+        self.start_patch('yak_server.__main__.server_running', new=self.run_for_iterations(self.ITERATIONS))
 
     def run_for_iterations(self, count):
         def generator():
@@ -23,12 +27,39 @@ class TestMain(util.TestCase):
             yield False
         return generator().__next__
 
-    def test_main(self):
+    def test_main_calls_correct_functons(self):
         __main__.main()
 
-        print(self.mock_usb.mock_calls)
+        switch_device_call = call.core.find(idProduct=0x04d8, idVendor=0x5901)
+        AC_device_call = call.core.find(idProduct=0x04d8, idVendor=0x5901)
 
-        self.mock_usb.assert_has_calls([
-            unittest.mock.call.core.find(idProduct=22784, idVendor=1240),
-            unittest.mock.call.core.find(idProduct=22785, idVendor=1240).detach_kernel_driver(0),
-            ], any_order=False)
+        switch_device = self.mock_usb.core.find(idProduct=0x04d8, idVendor=0x5901)
+        AC_device = self.mock_usb.core.find(idProduct=0x04d8, idVendor=0x5901)
+
+        switch_conf = switch_device.get_active_configuration()
+        switch_interface = switch_conf.interfaces()[0]
+        switch_endpoint = switch_interface.endpoints()[0]
+
+        AC_conf = AC_device.get_active_configuration()
+        AC_interface = AC_conf.interfaces()[0]
+        AC_endpoint = AC_interface.endpoints()[0]
+
+        manager_mock = unittest.mock.MagicMock()
+        manager_mock.switch_endpoint = switch_endpoint
+        manager_mock.AC_endpoint = AC_endpoint
+
+        self.mock_usb.assert_has_calls([call.core.find(idProduct=0x05d8, idVendor=0x5900).detach_kernel_driver(0)])
+        self.mock_usb.assert_has_calls([call.util.claim_interface(switch_device, 0)])
+
+        self.mock_usb.assert_has_calls([call.core.find(idProduct=0x05d8, idVendor=0x5901).detach_kernel_driver(0)])
+        self.mock_usb.assert_has_calls([call.util.claim_interface(switch_device, 0)])
+
+        read_calls = [True for call in switch_endpoint.mock_calls if call == call.read(1)]
+        number_of_read_calls = sum(read_calls)
+        self.assertEqual(number_of_read_calls, self.ITERATIONS)
+
+        read_data = switch_endpoint.read(1)
+
+        write_calls = [True for call in AC_endpoint.mock_calls if call == call.write(read_data)]
+        number_of_write_calls = sum(write_calls)
+        self.assertEqual(number_of_write_calls, self.ITERATIONS)
