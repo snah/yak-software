4477c4b0242396c5a77a93e7ce1828ea2115d706
Hans Maree <hans.maree@gmail.com>
2017-06-24 16:56:31 +0200

    First attempt of functional test.

---
+++ yak_server/test/functional/test_single_switch_single_lamp.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+
+import copy
+import unittest
+import unittest.mock
+
+from test import util
+
+import yak_server.__main__
+
+
+class TestSingleSwitchSingleLamp(util.TestCase):
+    """Test using a single switch to turn on and off a single lamp.
+    
+    Only a single switch and a single lamp are connected. Test that
+    the switch can be used to turn the lamp on and off.
+    """
+
+    ITERATIONS = 3
+    
+    def setUp(self):
+        self.start_patch('yak_server.__main__.usbdevice.find', side_effect=self.map_mock_device)
+        self.start_patch('yak_server.__main__.server_running', new=self.run_for_iterations(self.ITERATIONS))
+        self.mock_switch_device = unittest.mock.Mock()
+        self.mock_AC_device = unittest.mock.Mock()
+
+    def map_mock_device(self, **kwargs):
+        device_map = {(0x04d8, 0x5900): (self.mock_switch_device, ),
+                      (0x04d8, 0x5901): (self.mock_AC_device, )}
+        return device_map[(kwargs['vendor_id'], kwargs['product_id'])]
+
+    def run_for_iterations(self, count):
+        def generator():
+            for i in range(count):
+                yield True
+            yield False
+        return generator().__next__
+
+    def assert_writes_correct_data(self, data):
+        self.assertEqual(data, next(self.expected_data_iterator))
+
+    def assert_all_data_was_read(self):
+        with self.assertRaises(StopIteration):
+            next(self.input_data_iterator)
+
+    def test_single_switch_single_lamp(self):
+        self.input_data_iterator = iter(['on', 'off', 'on'])
+        self.expected_data_iterator = iter(['on', 'off', 'on'])
+        self.mock_switch_device.read.side_effect = self.input_data_iterator
+        self.mock_AC_device.write.side_effect = self.assert_writes_correct_data
+
+        yak_server.__main__.main()
+    
+        self.assert_all_data_was_read()
