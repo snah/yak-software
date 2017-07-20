98b077fc62a67ab78938a39be9cffe67dc879745
Hans Maree <hans.maree@gmail.com>
2017-07-20 13:15:33 +0200

    Filled in setup.py.

--- yak_server/setup.py
+++ yak_server/setup.py
@@ -11,8 +11,8 @@ with open(README_PATH, encoding='utf-8') as f:
 
 setuptools.setup(
     name='yak_server',
-    version='', #TODO
-    description='', #TODO
+    version='0.1',
+    description='Server for yak home automation system.',
     long_description=LONG_DESCRIPTION,
 
     url='https://github.com/snah/yak_server',
@@ -20,16 +20,15 @@ setuptools.setup(
     author='Hans Maree',
     author_email='hans.maree@gmail.com',
 
-    license='', #TODO
+    license='GNU General Public License v3.0',
 
     classifiers=['Development Status :: 4 - Beta',
                  'Intended Audience :: Developers',
-                 'Topic :: Software Development',
                  'Programming Language :: Python :: 3',
-                 #TODO
+                 'Topic :: Home Automation'
                  ],
 
-    keywords='', #TODO
+    keywords='',
 
     packages=['yak_server'],
 )
---
+++ yak_server/yak_server/__init__.py
@@ -0,0 +1,1 @@
+"""Server for yak home automation system."""
