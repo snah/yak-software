eadef50061cc9b8d9a9a079395caabe6699503bb
Hans Maree <hans.maree@gmail.com>
2017-08-13 14:50:58 +0200

    Made FakeSwitchDevice thread safe for functional tests.

--- yak_server/test/doubles/fake_switch_device.py
+++ yak_server/test/doubles/fake_switch_device.py
@@ -1,26 +1,36 @@
+import queue
+
 from yak_server import usbdevice
 
 
 class FakeSwitchDevice:
     def __init__(self):
-        self.read_buffer = b''
+        self._read_buffer = b''
+        self._read_queue = queue.Queue()
 
     def connect(self):
         pass
 
     def flush(self):
-        pass
+        self._read_buffer = b''
+        while not self._read_queue.empty():
+            self._read_queue.get()
 
     def read(self, number_of_bytes):
-        if self.read_buffer:
-            response = self.read_buffer[:number_of_bytes]
-            self.read_buffer = self.read_buffer[len(response):]
+        self._update_read_buffer()
+        if self._read_buffer:
+            response = self._read_buffer[:number_of_bytes]
+            self._read_buffer = self._read_buffer[len(response):]
             return response
         else:
             raise usbdevice.USBError('No data available')
 
     def press_button(self):
-        self.read_buffer += b'\x01'
+        self._read_queue.put(b'\x01')
 
     def release_button(self):
-        self.read_buffer += b'\x00'
+        self._read_queue.put(b'\x00')
+
+    def _update_read_buffer(self):
+        while not self._read_queue.empty():
+            self._read_buffer += self._read_queue.get()
